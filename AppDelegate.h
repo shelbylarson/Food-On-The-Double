c72dd2aa2f79dc0dd4f728100d9d48fced308c7f
shelbylarson <salshelbylarson@gmail.com>
2016-11-04 11:36:13 -0500

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Pasan Premaratne on 1/8/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
